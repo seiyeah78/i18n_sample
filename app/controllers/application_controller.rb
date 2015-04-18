ef915d01da1dbd96901a2885a77c828d16e36044
SeiyaOuchi <ketcoding@gmail.com>
2015-04-18 23:14:59 +0900

    add some files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  #before_action :redirect_to_default_locale
+  before_action :redirect_to_default_locale
   before_action :setup_locale
+
+  rescue_from ActiveRecord::RecordNotFound, with: :error404
 
   def default_url_options(options = {})
     { locale: I18n.locale }.merge(options)
@@ -13,7 +15,7 @@ class ApplicationController < ActionController::Base
   private
 
   def redirect_to_default_locale
-    redirect_to root_path if params[:locale].blank?
+    redirect_to controller: params[:controller], action: params[:action], locale: I18n.default_locale if params[:locale].blank?
   end
 
   def setup_locale
@@ -24,5 +26,9 @@ class ApplicationController < ActionController::Base
         I18n.default_locale
       end
     I18n.locale = locale
+  end
+
+  def error404
+    render file: Rails.root.join('public/404.html'), status: 404, layout: false, content_type: 'text/html'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "/" => "users#index"
   scope "/:locale", constraints: Constraints::WhiteList.new do
     root controller: :users, action: :index
     resources :users
